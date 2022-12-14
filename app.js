dfb932331dc2cd3b761bd3c0855b527d90eb3682
EG-DEV5 <egypt.team.dev5@gmail.com>
2022-12-14 16:16:35 +0200

    clean up app.js

--- app.js
+++ app.js
@@ -15,35 +15,6 @@ app.use("/api/devices", devices)
 
 app.use(errorHandler)
 app.get("/", (req, res) => {
-    res.send('<h1> Varma Attack</h1>')
+    res.send('<h1> Varma Track</h1>')
 })
 app.listen(PORT, () => console.log(`running on port ${PORT}`))
-// const model = require("./models/device")
-// model.create({
-//     VehicleID: 123,
-//     Name: 'Veh',
-//     PlateNumber: 'PN',
-//     FuelType: 'بنزين 95',
-//     SpeedLimit: 20
-// }).then(d => console.log(d)).catch(e => console.log(e.message))
-
-// model.create({
-//     UserName: 'Mostafa',
-//     Password: 'pass12312',
-//     PhoneNumber: '01121299686',
-//     Address: 'Ma3aaaaaaaaadi',
-//     Email: "mo@example.com",
-//     Role: "user"
-// }).then(d => console.log(d)).catch(e => console.log(e.message))
-
-// model.create({
-//     IdentityNumber: '123inh64',
-//     LicenceNumber: '2312',
-//     ParentID: '6397201badb488a68cdf1d01',
-//     VehicleID: '6396ef808ab814e1bfdc88ca'
-// }).then(d => console.log(d)).catch(e => console.log(e.message))
-
-// model.create({
-//     DeviceName: 'first Device',
-//     SerialNumber: '123456'
-// }).then(d => console.log(d)).catch(e => console.log(e.message))
