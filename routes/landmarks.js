1b697de09d52c70ad9ae3f9920c19f1feb61bbed
Mostafa-Fawzy <egypt.team.dev6@gmail.com>
2022-12-28 13:05:43 +0200

    delete&update landmarks

--- routes/landmarks.js
+++ routes/landmarks.js
@@ -1,11 +1,16 @@
-const { addLandmark, getLandmarks } = require("../controllers/landmarks")
+const { addLandmark, getLandmarks, deleteLandmark, updateLandmark } = require("../controllers/landmarks")
 const router = require("express").Router()
 const { isAuthenticated } = require("../middlewares/auth")
 const { uploadPhoto } = require("../middlewares/upload")
 let type = uploadPhoto.single('Image');
 
+
+router.use(isAuthenticated)
+
 router.route('/')
-    .get(isAuthenticated, getLandmarks)
-    .post(isAuthenticated, type, addLandmark)
+    .get(getLandmarks)
+    .post(type, addLandmark)
+
+router.route('/:landId').delete(deleteLandmark).put(type, updateLandmark)
 
 module.exports = router
